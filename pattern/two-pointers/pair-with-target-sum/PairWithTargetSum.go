50c412697b7f09affa57e7421fa54a5a4521a30c
baconyao <baconyao@baconyaode-MacBook-Air.local>
2022-02-09 10:44:32 +0800

    Add PairWithTargetSum solution. Close #14

--- pattern/two-pointers/pair-with-target-sum/PairWithTargetSum.go
+++ pattern/two-pointers/pair-with-target-sum/PairWithTargetSum.go
@@ -1,6 +1,20 @@
 package pairwithtargetsum
 
 func PairWithTargetSum(target int, input []int) []int {
+	// the pointer of start and end position
+	sp := 0
+	ep := len(input) - 1
+
+	for sp < ep {
+		sum := input[sp] + input[ep]
+		if sum == target {
+			return []int{sp, ep}
+		} else if sum > target {
+			ep -= 1
+		} else {
+			sp += 1
+		}
+	}
 
 	return []int{-1, -1}
 }
--- pattern/two-pointers/pair-with-target-sum/PairWithTargetSum_test.go
+++ pattern/two-pointers/pair-with-target-sum/PairWithTargetSum_test.go
@@ -17,6 +17,7 @@ func TestPairWithTargetSum(t *testing.T) {
 	}{
 		{6, []int{1, 2, 3, 4, 6}, []int{1, 3}, "The numbers at index 1 and 3 add up to 6: 2+4=6"},
 		{11, []int{2, 5, 9, 11}, []int{0, 2}, "The numbers at index 0 and 2 add up to 11: 2+9=11"},
+		{18, []int{2, 5, 9, 11}, []int{-1, -1}, "There's no set to match 18"},
 	}
 
 	for _, e := range theTests {
