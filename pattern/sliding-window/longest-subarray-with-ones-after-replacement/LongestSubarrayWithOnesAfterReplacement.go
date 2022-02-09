1bc018c9eddd0c01701299601be2a3ad11066fa7
baconyao <baconyao@baconyaode-MacBook-Air.local>
2022-02-09 09:45:47 +0800

    Add LongestSubarrayWithOnesAfterReplacement solution. Close #9

--- pattern/sliding-window/longest-subarray-with-ones-after-replacement/LongestSubarrayWithOnesAfterReplacement.go
+++ pattern/sliding-window/longest-subarray-with-ones-after-replacement/LongestSubarrayWithOnesAfterReplacement.go
@@ -1,5 +1,28 @@
 package longestsubarraywithonesafterreplacement
 
+import (
+	"github.com/baconYao/WayToHell/utils"
+)
+
 func LongestSubarrayWithOnesAfterReplacement(k int, arr []int) int {
-	return -1
+	windowStart := 0
+	oneCount := 0
+	longestSubarray := 0
+
+	for windowEnd, number := range arr {
+		if number == 1 {
+			oneCount += 1
+		}
+		// 確認 window 內的 0 的數量是否多於 k
+		for windowEnd-windowStart+1-oneCount > k {
+			// number at start pointer
+			ns := arr[windowStart]
+			if ns == 1 {
+				oneCount -= 1
+			}
+			windowStart += 1
+		}
+		longestSubarray = utils.Max(longestSubarray, windowEnd-windowStart+1)
+	}
+	return longestSubarray
 }
