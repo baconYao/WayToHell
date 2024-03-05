9b1b583447220d59c89234669fb47d69fbfb7362
baconyao <bacon735392@gmail.com>
2024-03-05 20:57:45 +0800

    Solve happy numbers

--- pattern/fast-and-slow-pointers/happy-numbers/HappyNumber.go
+++ pattern/fast-and-slow-pointers/happy-numbers/HappyNumber.go
@@ -1,6 +1,30 @@
 package happynumbers
 
 func IsHappyNumber(num int) bool {
+	slow, fast := num, sumOfSquaredDigits(num)
+	for fast != 1 && fast != slow {
+		slow = sumOfSquaredDigits(slow)
+		fast = sumOfSquaredDigits(sumOfSquaredDigits(fast))
+	}
+	return fast == 1
+}
 
-	return false
+// pow calculates the power of the given digit
+func pow(digit int, power int) int {
+	res := 1
+	for i := 0; i < power; i++ {
+		res = res * digit
+	}
+	return res
+}
+
+// sumOfSquaredDigits is a helper function that calculates the sum of squared digits
+func sumOfSquaredDigits(number int) int {
+	totalSum := 0
+	for number > 0 {
+		digit := number % 10
+		number = number / 10
+		totalSum += pow(digit, 2)
+	}
+	return totalSum
 }
