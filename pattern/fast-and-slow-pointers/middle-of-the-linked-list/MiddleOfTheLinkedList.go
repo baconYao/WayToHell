b94c72a7f87505534c9db866b9c3273a5fd6a482
baconyao <bacon735392@gmail.com>
2024-03-06 22:42:50 +0800

    Add Middle of the Linked List Problem

---
+++ pattern/fast-and-slow-pointers/middle-of-the-linked-list/MiddleOfTheLinkedList.go
@@ -0,0 +1,7 @@
+package middleofthelinkedlist
+
+func GetMiddleNode(head *LinkedListNode) *LinkedListNode {
+
+	// Replace this placeholder return statement with your code
+	return new(LinkedListNode)
+}
