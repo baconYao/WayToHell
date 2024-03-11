3dbf8947727168f66531ddb6bc33d01707a3c53a
baconyao <bacon735392@gmail.com>
2024-03-11 21:03:04 +0800

    Solve Find Maximum in Sliding Window, but it's the worst solution (Time Limit Exceeded)

--- pattern/sliding-window/find-maximum-in-sliding-window/FindMaximumInSlidingWindow.go
+++ pattern/sliding-window/find-maximum-in-sliding-window/FindMaximumInSlidingWindow.go
@@ -1,6 +1,30 @@
 package findmaximuminslidingwindow
 
+// Time Limit Exceeded
+
 func FindMaxSlidingWindow(nums []int, w int) []int {
 	result := make([]int, 0)
+
+	if w > len(nums) {
+		w = len(nums)
+	}
+
+	front := 0
+	for rear := w - 1; rear < len(nums); rear++ {
+		currMaxInWindow := findMaximunNum(nums, front, rear)
+		result = append(result, currMaxInWindow)
+		front += 1
+	}
+
 	return result
+}
+
+func findMaximunNum(nums []int, front, rear int) int {
+	max := -99999
+	for i := front; i <= rear; i++ {
+		if nums[i] > max {
+			max = nums[i]
+		}
+	}
+	return max
 }
