07cd713706c77c4d4c0c1eed413901ba3b75d9d5
baconyao <bacon735392@gmail.com>
2024-04-02 23:45:27 +0800

    Solve Longest Repeating Character Replacement problem

--- pattern/sliding-window/longest-repeating-character-replacement/longestRepeatingCharacterReplacement.go
+++ pattern/sliding-window/longest-repeating-character-replacement/longestRepeatingCharacterReplacement.go
@@ -1,5 +1,26 @@
 package minimumwindowsubsequence
 
+import (
+	"github.com/baconYao/WayToHell/utils"
+)
+
 func CharacterReplacement(s string, k int) int {
-	return 0
+	charFrequency := make(map[string]int)
+	lengthOfMaxSubstring := -1
+	mostFreqChar := 0
+	left := 0
+	for right, char := range s {
+		c := string(char)
+		charFrequency[c] += 1
+		// Obtain the count of the most frequently occuring characters
+		mostFreqChar = utils.Max(mostFreqChar, charFrequency[c])
+
+		if right-left+1-mostFreqChar > k {
+			charFrequency[string(s[left])] -= 1
+			left += 1
+		}
+		lengthOfMaxSubstring = utils.Max(right-left+1, lengthOfMaxSubstring)
+	}
+
+	return lengthOfMaxSubstring
 }
