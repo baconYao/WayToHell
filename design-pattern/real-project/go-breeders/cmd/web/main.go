7e26db64be62f28ccdf9b8c4b9b637752a6328eb
baconyao <bacon735392@gmail.com>
2024-04-07 22:49:01 +0800

    Init go-breeders real project in Design Pattern

---
+++ design-pattern/real-project/go-breeders/cmd/web/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+)
+
+const port = ":4000"
+
+type application struct {
+}
+
+func main() {
+	app := application{}
+
+	srv := &http.Server{
+		Addr:              port,
+		Handler:           app.routes(),
+		IdleTimeout:       30 * time.Second,
+		ReadTimeout:       30 * time.Second,
+		ReadHeaderTimeout: 30 * time.Second,
+		WriteTimeout:      30 * time.Second,
+	}
+
+	fmt.Println("Starting web application on port", port)
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
---
+++ design-pattern/real-project/go-breeders/go.mod
@@ -0,0 +1,5 @@
+module go-breeders
+
+go 1.21.3
+
+require github.com/go-chi/chi/v5 v5.0.12 // indirect
