bc769f16c5076d5e02d1ed3f600a0c02b3d8d8cb
baconyao <bacon735392@gmail.com>
2024-04-08 19:49:50 +0800

    Add render and templates

--- design-pattern/real-project/go-breeders/cmd/web/main.go
+++ design-pattern/real-project/go-breeders/cmd/web/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 	"time"
@@ -10,10 +12,21 @@ import (
 const port = ":4000"
 
 type application struct {
+	templateMap map[string]*template.Template
+	config      appConfig
+}
+
+type appConfig struct {
+	useCache bool
 }
 
 func main() {
-	app := application{}
+	app := application{
+		templateMap: make(map[string]*template.Template),
+	}
+
+	flag.BoolVar(&app.config.useCache, "cache", false, "Use template cache")
+	flag.Parse()
 
 	srv := &http.Server{
 		Addr:              port,
