ae4e503b8470f5b53b1445c4813f538ca7d7645c
baconyao <bacon735392@gmail.com>
2024-04-20 16:55:27 +0800

    Add Model and handler for querying All dog

--- design-pattern/real-project/go-breeders/cmd/web/main.go
+++ design-pattern/real-project/go-breeders/cmd/web/main.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"flag"
 	"fmt"
+	"go-breeders/models"
 	"html/template"
 	"log"
 	"net/http"
@@ -16,6 +17,7 @@ type application struct {
 	templateMap map[string]*template.Template
 	config      appConfig
 	DB          *sql.DB
+	Models      models.Models
 }
 
 type appConfig struct {
@@ -39,6 +41,7 @@ func main() {
 	}
 
 	app.DB = db
+	app.Models = *models.New(db)
 
 	srv := &http.Server{
 		Addr:              port,
--- design-pattern/real-project/go-breeders/models/models.go
+++ design-pattern/real-project/go-breeders/models/models.go
@@ -1,6 +1,23 @@
 package models
 
-import "time"
+import (
+	"database/sql"
+	"time"
+)
+
+var db *sql.DB
+
+type Models struct {
+	DogBreed DogBreed
+}
+
+func New(conn *sql.DB) *Models {
+	db = conn
+
+	return &Models{
+		DogBreed: DogBreed{},
+	}
+}
 
 type DogBreed struct {
 	ID               int    `json:"id"`
@@ -9,9 +26,13 @@ type DogBreed struct {
 	WeightHighLbs    int    `json:"weight_high_lbs"`
 	AverageWeight    int    `json:"average_weight"`
 	Lifespan         int    `json:"average_lifespan"`
-	Details          int    `json:"details"`
+	Details          string `json:"details"`
 	AlternateNames   string `json:"alternate_names"`
 	GeographicOrigin string `json:"geographic_origin"`
+}
+
+func (d *DogBreed) All() ([]*DogBreed, error) {
+	return d.AllDogBreeds()
 }
 
 type CatBreed struct {
@@ -21,7 +42,7 @@ type CatBreed struct {
 	WeightHighLbs    int    `json:"weight_high_lbs"`
 	AverageWeight    int    `json:"average_weight"`
 	Lifespan         int    `json:"average_lifespan"`
-	Details          int    `json:"details"`
+	Details          string `json:"details"`
 	AlternateNames   string `json:"alternate_names"`
 	GeographicOrigin string `json:"geographic_origin"`
 }
