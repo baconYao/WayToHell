81114ae0478dcc5d023a3e5950a45c72a4a9fa63
baconyao <bacon735392@gmail.com>
2025-03-22 00:50:52 +0800

    Add better solution to leetcode - 457

--- leetcode/457-Circular_Array_Loop/main.py
+++ leetcode/457-Circular_Array_Loop/main.py
@@ -38,3 +38,32 @@ class Solution:
         if pre_direction != curr_direction or curr_value % size_of_nums == 0:
             return True
         return False
+
+
+class BetterSolution:
+    def circularArrayLoop(self, nums: List[int]) -> bool:
+        n, visited = len(nums), set()  # 獲取數組長度，初始化已訪問集合
+        for i in range(n):  # 遍歷每個元素作為起點
+            if i not in visited:
+                local_s = set()  # 當前起點的本地集合
+                while True:
+                    # 如果當前索引已在本地集合中，表示形成循環
+                    if i in local_s:
+                        return True
+
+                    # 如果當前索引已在全域已訪問集合中，跳出當前循環
+                    if i in visited:
+                        break
+
+                    # 將當前索引標記為已訪問
+                    visited.add(i)
+                    local_s.add(i)
+
+                    # 計算下一個索引
+                    prev, i = i, (i + nums[i]) % n
+
+                    # 如果下一個索引和當前索引相同，或方向不一致，中斷
+                    if prev == i or (nums[i] > 0) != (nums[prev] > 0):
+                        break
+
+        return False
