70ecc120d9695ae0ac5e0ad61d7018004398f135
baconyao <bacon735392@gmail.com>
2025-08-23 16:17:46 -0400

    feat: Solve Leetcode 287 with slow fast pointer

--- leetcode/287-Find_the_Duplicate_Number/main.go
+++ leetcode/287-Find_the_Duplicate_Number/main.go
@@ -14,3 +14,23 @@ package findtheduplicatenumber
 
 //     return 0
 // }
+
+func findDuplicate(nums []int) int {
+	slow := nums[0]
+	fast := nums[0]
+	for {
+		slow = nums[slow]
+		fast = nums[nums[fast]]
+		if slow == fast {
+			break
+		}
+	}
+	slow = nums[0]
+	for slow != fast {
+		slow = nums[slow]
+		fast = nums[fast]
+	}
+
+	return slow
+
+}
